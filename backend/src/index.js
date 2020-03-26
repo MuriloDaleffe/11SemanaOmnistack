2415c0e4d330546699f72a5bb3533bb4a1507a7e
Murilo Daleffe <42449809+MuriloDaleffe@users.noreply.github.com>
2020-03-26 19:55:46 -0300

    Back-End da aplicação finalizado

---
+++ backend/src/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+use.app(cors());
+
+app.use(express.json());
+
+app.use(routes);
+
+/**
+ * Rota / Recurso
+ */
+
+/**
+* Metodos HTPP:
+* GET: buscar/listar uma informacao do backend
+* POST: criar uma informacao no backend
+* PUT: alterar uma informacao no backend
+* DELETE: deletar uma informacao no backend
+*/
+
+/**
+ * Tipos de Parametros
+ * 
+ * Query Params: Parametros nomeados enviados na rota apos "?" para filtros, paginacao, ect...
+ * Route Params: Parametros utilizados para indentificar recursos
+ * Request Body: Corpo da requsicao, utilizado para criar ou alterar recursos
+ */
+
+ /**
+  * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+  * NoSQL: MongoDB, CouchDB
+  */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
+
+
+
+app.listen(3333);
